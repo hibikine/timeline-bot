e461803e26223f7440dfa984060bcb06375c3a2c
Hibikine Kage <goodsmile314159@gmail.com>
2020-04-07 14:56:08 +0900

    add timeschannel get logic

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import { Robot, Adapter } from "hubot";
+import { WebClient, WebAPICallResult } from "@slack/client";
 namespace hubot {}
 interface SlackAdapter extends Adapter {
+  options: {
+    token: string;
+  };
   client: { rtm: any };
 }
+interface Channel {
+  id: string;
+  name: string;
+}
+interface ChannelsListResponse extends WebAPICallResult {
+  channels: Channel[];
+}
 
-module.exports = (robot: Robot<SlackAdapter>) => {
-  robot.hear(/.*?/i, (msg) => {
+module.exports = async (robot: Robot<SlackAdapter>) => {
+  /*robot.hear(/.*?/i, (msg) => {
     const channel = msg.envelope.room;
     const room_name = robot.adapter.client.rtm;
-  });
+  });*/
+  const web = new WebClient(robot.adapter.options.token);
+  try {
+    const timesChannels = ((await web.channels.list()) as ChannelsListResponse).channels.find(
+      (channel) => channel.name.startsWith("times_")
+    );
+  } catch (e) {
+    robot.logger.error(e.massage);
+  }
 };
 
 const USER_IMAGES = "userImages";
