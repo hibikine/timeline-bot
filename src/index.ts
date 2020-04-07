8506c6fdba87e3bb583f3b0a462c28bbc5c64ec3
Hibikine Kage <goodsmile314159@gmail.com>
2020-04-07 15:23:13 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import { Robot, Adapter } from "hubot";
+import { Robot, Adapter, Message } from "hubot";
 import { WebClient, WebAPICallResult } from "@slack/client";
-namespace hubot {}
+interface SlackMessage extends Message {
+  rawmessage: {
+    channel: Channel;
+    attachments: Attachment[];
+    thread_ts: any;
+    ts: string;
+  };
+}
 interface SlackAdapter extends Adapter {
   options: {
     token: string;
   };
+  customMessage(data: CustomMessageData): void;
   client: { rtm: any };
 }
+interface CustomMessageData {
+  attachments?: Attachment[];
+  content?: Attachment[];
+  text?: string;
+  username?: string;
+  channel?: string;
+  message?: CustomMessage;
+  icon_url?: string;
+  icon_emoji?: string;
+}
+interface CustomMessage {
+  envelope?: {
+    room: string;
+  };
+  room?: string;
+}
+interface Attachment {}
+
 interface Channel {
   id: string;
   name: string;
@@ -16,15 +42,26 @@ interface ChannelsListResponse extends WebAPICallResult {
 }
 
 module.exports = async (robot: Robot<SlackAdapter>) => {
-  /*robot.hear(/.*?/i, (msg) => {
-    const channel = msg.envelope.room;
-    const room_name = robot.adapter.client.rtm;
-  });*/
-  const web = new WebClient(robot.adapter.options.token);
   try {
-    const timesChannels = ((await web.channels.list()) as ChannelsListResponse).channels.find(
-      (channel) => channel.name.startsWith("times_")
-    );
+    const web = new WebClient(robot.adapter.options.token);
+    const channels = ((await web.channels.list()) as ChannelsListResponse)
+      .channels;
+    const timesChannels: string[] = channels
+      .filter((channel) => channel.name.startsWith("times_"))
+      .map((channel) => channel.id);
+    const timelineChannel: string = channels.find(
+      (channel) => channel.name === "timeline"
+    ).id;
+
+    robot.hear(/.*?/i, (res) => {
+      const { rawmessage } = (res.message as any) as SlackMessage;
+      if (timesChannels.includes(rawmessage.channel.id)) {
+        robot.adapter.customMessage({
+          channel: timelineChannel,
+          username: rawmessage.attachments,
+        });
+      }
+    });
   } catch (e) {
     robot.logger.error(e.massage);
   }
