93004a72f9433c671d3949211cac2711b6935b93
Hibikine Kage <goodsmile314159@gmail.com>
2020-04-08 16:48:27 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { Robot, Adapter, Message } from "hubot";
 import { WebClient, WebAPICallResult } from "@slack/client";
+type SlackFile = {
+  url_private: string;
+};
 interface SlackMessage extends Message {
-  rawmessage: {
-    channel: Channel;
+  rawMessage: {
+    files?: SlackFile[];
+    channel: string;
     attachments: Attachment[];
     thread_ts: any;
     ts: string;
+    user: {
+      id: string;
+      slack: {
+        profile: {
+          image_192: string;
+        };
+      };
+      name: string;
+      real_name: string;
+    };
+    icon_url: string;
+    message: any;
+    text: string;
   };
 }
 interface SlackAdapter extends Adapter {
@@ -41,25 +58,52 @@ interface ChannelsListResponse extends WebAPICallResult {
   channels: Channel[];
 }
 
+const getTimesChannel = async (robot: Robot<SlackAdapter>) => {
+  const web = new WebClient(robot.adapter.options.token);
+  const channels = ((await web.channels.list()) as ChannelsListResponse)
+    .channels;
+  return channels
+    .filter((channel) => channel.name.startsWith("times_"))
+    .map((channel) => channel.id);
+};
 module.exports = async (robot: Robot<SlackAdapter>) => {
   try {
     const web = new WebClient(robot.adapter.options.token);
     const channels = ((await web.channels.list()) as ChannelsListResponse)
       .channels;
-    const timesChannels: string[] = channels
-      .filter((channel) => channel.name.startsWith("times_"))
-      .map((channel) => channel.id);
     const timelineChannel: string = channels.find(
       (channel) => channel.name === "timeline"
     ).id;
+    robot.logger.info(timelineChannel);
 
-    robot.hear(/.*?/i, (res) => {
-      const { rawmessage } = (res.message as any) as SlackMessage;
-      if (timesChannels.includes(rawmessage.channel.id)) {
-        robot.adapter.customMessage({
-          channel: timelineChannel,
-          username: rawmessage.attachments,
-        });
+    robot.hear(/.*?/i, async (res) => {
+      const { rawMessage } = (res.message as any) as SlackMessage;
+      robot.logger.info(rawMessage);
+      robot.logger.info(res);
+      robot.logger.info(rawMessage.user.slack.profile);
+      if ((await getTimesChannel(robot)).includes(rawMessage.channel)) {
+        let files = undefined;
+        if (rawMessage.files) {
+          files = rawMessage.files.map((v) => ({
+            fallback: "image",
+            image_url: v.url_private,
+            thumb_url: v.url_private,
+          }));
+        }
+        if (rawMessage.files.length)
+          robot.send(
+            {
+              room: timelineChannel,
+            } as any,
+            {
+              as_user: false,
+              channel: timelineChannel,
+              username: rawMessage.user.real_name,
+              icon_url: rawMessage.user.slack.profile.image_192,
+              attachments: files,
+              text: rawMessage.text,
+            } as any
+          );
       }
     });
   } catch (e) {
@@ -67,7 +111,7 @@ module.exports = async (robot: Robot<SlackAdapter>) => {
   }
 };
 
-const USER_IMAGES = "userImages";
+/*const USER_IMAGES = "userImages";
 type UserImages = {
   [key: string]: string;
 };
@@ -81,7 +125,8 @@ function reloadUserImages(robot: Robot, userId: number) {
   {
     const userImages: UserImages = robot.brain.get(USER_IMAGES);
     if (userId in userImages) {
-      const newUserImages = robot.brain.set(USER_IMAGES, newUserImages);
+      const newUserImages = {...userImages, [userId]: user}
+        robot.brain.set(USER_IMAGES, newUserImages);
     }
   }
-}
+}*/
